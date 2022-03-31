70f68f1f9cf983f4597f6779b5a8eb943f37b2fb
jacks-trapp <89658563+jacks-trapp@users.noreply.github.com>
2022-03-30 19:54:46 -0500

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        //constructors
+        public Rectangle()
+        {
+            new Rectangle(defaultHeight, defaultWidth);
+        }
+        public Rectangle(double height, double width)
+        {
+            this._height = height;
+            this._width = width;
+        }
+
+        //destructor
+        ~Rectangle()
+        {
+
+        }
+
+        //fields
+        private double _height;
+        private double _width;
+        public const double defaultHeight = 0;
+        public const double defaultWidth = 0;
+
+        internal double whatever = 6;
+
+        //properties
+        public Color Color { get; set; }
+
+        public double Height { get; set; }
+        //{
+        //    get { return height; }
+
+        //    set { height = value; }           
+        //}
+
+        public double Width
+        {
+            get { return _width; }
+
+            set { _width = value; }
+        }
+
+
+        //methods
+        public static double CalculateArea(double height, double width)
+        {
+            return height * width;
+        }
+        public override double CalculateArea()
+        {
+            return CalculateArea(_height, _width);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            throw new NotImplementedException();
+        }
+
+        // comparing two rectangles
+        public static bool operator < (Rectangle self, Rectangle other)
+        {
+            return self.CalculatePerimeter() <other.CalculatePerimeter();
+        }
+
+        public static bool operator <=(Rectangle self, Rectangle other)
+        {
+            return self.CalculatePerimeter() <= other.CalculatePerimeter();
+        }
+
+        public static bool operator > (Rectangle self, Rectangle other)
+        {
+            return self.CalculatePerimeter() >other.CalculatePerimeter();
+        }
+
+        public static bool operator >=(Rectangle self, Rectangle other)
+        {
+            return self.CalculatePerimeter() >= other.CalculatePerimeter();
+        }
+
+        public static bool operator ==(Rectangle self, Rectangle other)
+        {
+            return self.CalculatePerimeter() == other.CalculatePerimeter();
+        }
+
+        public static bool operator !=(Rectangle self, Rectangle other)
+        {
+            return self.CalculatePerimeter() != other.CalculatePerimeter();
+        }
+    }
+}
