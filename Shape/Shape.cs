c2437e309b8876be96ee2f228c5422917462d539
jacks-trapp <jacks_trapp@hotmail.com>
2022-04-01 16:00:04 -0500

    initial commit

---
+++ Shape/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public abstract class Shape
+    {
+        // test for pushing to github
+        static void Main()
+        {
+            Console.WriteLine("pushing to github");
+            Console.WriteLine("change has been made");
+        }
+
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+    }
+}
